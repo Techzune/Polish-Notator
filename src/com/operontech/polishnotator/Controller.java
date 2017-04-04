3f4c265a9284d0e0fb9745c310cb6416349d914c
techzune <techzune@operontech.com>
2017-04-04 17:11:44 -0500

    Initial commit

---
+++ src/com/operontech/polishnotator/Controller.java
@@ -0,0 +1,4 @@
+package com.operontech.polishnotator;
+
+public class Controller {
+}
---
+++ src/com/operontech/polishnotator/Main.java
@@ -0,0 +1,23 @@
+package com.operontech.polishnotator;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+		primaryStage.setTitle("Polish Notator");
+		primaryStage.setScene(new Scene(root, 563, 233));
+		primaryStage.setResizable(false);
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
