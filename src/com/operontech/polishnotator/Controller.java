ab29ab70a1667f911b54a401a6da75f10c753181
techzune <techzune@operontech.com>
2017-04-04 20:51:42 -0500

    Visuals and controllers added

--- src/com/operontech/polishnotator/Controller.java
+++ src/com/operontech/polishnotator/Controller.java
@@ -1,4 +1,45 @@
 package com.operontech.polishnotator;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+	@FXML
+	TextField textFieldInput;
+
+	@FXML
+	protected void handleConvert(ActionEvent e) {
+		System.out.println(e.getSource().toString());
+	}
+
+	@FXML
+	protected void actionButtonNot(ActionEvent e) {
+		addToInput('~');
+	}
+
+	@FXML
+	protected void actionButtonAnd(ActionEvent e) {
+		addToInput('•');
+	}
+
+	@FXML
+	protected void actionButtonOr(ActionEvent e) {
+		addToInput('∨');
+	}
+
+	@FXML
+	protected void actionButtonIfThen(ActionEvent e) {
+		addToInput('⊃');
+	}
+
+	@FXML
+	protected void actionButtonOnlyIf(ActionEvent e) {
+		addToInput('≡');
+	}
+
+	private void addToInput(char symbol) {
+		textFieldInput.setText(textFieldInput.getText() + symbol);
+	}
 }
